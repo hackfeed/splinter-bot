eae9a5fddae425f05391b1b351cb6142d61bc367
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-25 21:09:03 +0300

    Add error handling & refactor some code

--- bot.py
+++ bot.py
@@ -1,48 +1,51 @@
 import os
 import random
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
+import meta
 
+
+load_dotenv()
 TOKEN = os.getenv("TOKEN")
 bot = commands.Bot(command_prefix="!")
 
 
 @bot.command(name="split")
 @commands.has_role("Captain Flex")
-async def split_users(ctx, users_per_team, base_channel, team_a_channel, team_b_channel, is_strict="нестрого"):
+async def split_users(ctx, users_per_team, base_channel, team_a_channel, team_b_channel, is_strict=meta.NOT_STRICT):
     guild = ctx.guild
     base = discord.utils.get(guild.voice_channels, name=base_channel)
     team_a = discord.utils.get(guild.voice_channels, name=team_a_channel)
     team_b = discord.utils.get(guild.voice_channels, name=team_b_channel)
 
     if not base:
-        await ctx.send("❌ Не найден голосовой канал, из которого распределяются пользователи.")
+        await ctx.send(meta.NO_BASE_CHANNEL_MSG)
         return
     if not team_a:
-        await ctx.send("❌ Не найден голосовой канал, в который нужно распределить участников первой команды.")
+        await ctx.send(meta.NO_TEAM_A_CHANNEL_MSG)
         return
     if not team_b:
-        await ctx.send("❌ Не найден голосовой канал, в который нужно распределить участников второй команды.")
+        await ctx.send(meta.NO_TEAM_B_CHANNEL_MSG)
         return
 
     users = base.members
     users_per_team = int(users_per_team)
 
     if len(users) < users_per_team:
-        await ctx.send("❌ Невозможно распределить участников выбранным способом: количество пользователей меньше количества участников команды.")
+        await ctx.send(meta.TEAM_GT_USERS_MSG)
         return
 
-    if is_strict != "нестрого":
+    if is_strict != meta.NOT_STRICT:
         if len(users) != users_per_team * 2:
-            await ctx.send("❌ Невозможно поделить участников строго поровну. Деление не будет производиться.")
+            await ctx.send(meta.CANT_DIVIDE_STRICT_MSG)
             return
         else:
-            await ctx.send("🧱 Участники будут поделены строго поровну.")
+            await ctx.send(meta.STRICT_DIVISION_MSG)
     else:
-        await ctx.send("🧲 Деление будет производиться по остаточному принципу. Возможно, одна из команд не досчитается игроков :(")
+        await ctx.send(meta.NONSTRICT_DIVISION_MSG)
 
     team_a_membs = random.sample(users, users_per_team)
     team_b_membs = list(set(users) - set(team_a_membs))
@@ -53,4 +56,11 @@ async def split_users(ctx, users_per_team, base_channel, team_a_channel, team_b_
     for user_b in team_b_membs:
         await user_b.move_to(team_b)
 
-bot.run(TOKEN)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.MissingRole):
+        await ctx.send(meta.MISSING_ROLE_MSG)
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
